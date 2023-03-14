5e973f49926c9f56250134dc08a5c1e5c81f2361
Sameer <sameerejaz970@gmail.com>
2023-03-14 16:29:21 +0500

    ad

---
+++ src/app/calander/calander.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+@Component({
+  selector: 'app-calander',
+  templateUrl: './calander.component.html',
+  styleUrls: ['./calander.component.css']
+})
+export class CalanderComponent {
+
+
+  options: any;
+  
+}
